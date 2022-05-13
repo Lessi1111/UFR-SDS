b704d252bed720c77db163419beac84568afdaea
Brice <bricecoulibaly596@gmail.com>
2022-05-13 02:03:26 +0000

    gestion-administrateur

---
+++ authentique.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/ufr.css">
+    <title>Document</title>
+</head>
+<body>
+<section class="menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <img src="images/logo.jpg" alt="">
+                    <h2>UFR/SDS</h2>
+                </div>
+                <div class="col-4 mt-10">
+                   <a href="inscription.php">Créer un cmpte</a>
+                   <p>Connectez-vous</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+    <div class="container">
+
+<form action="./Authentique.php" method="post">
+    <div class="row mt-4">
+        <div class="col-2">
+            <label class="form-label">Email</label>
+        </div>
+        <div class="col-5">
+            <input type="email" class="form-control" name="nombre-1">
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-2">
+            <label class="form-label">MOT DE PASSE</label>
+        </div>
+        <div class="col-5">
+            <input type="password" class="form-control" name="nombre-2">
+        </div>
+    </div>
+    <button type="submit" class="btn btn-danger mt-4" name="conect">CONNECTION</button>
+</form>
+</div>
+    </section>
+</body>
+</html>
+
+<?php
+session_start();
+
+include ("./dbconnect.php");
+ if (isset($_POST['conect'])){
+ $mailconnect = htmlspecialchars($_POST['nombre-1']);
+ $mdpconnect = ($_POST['nombre-2']);
+
+ if (!empty($mailconnect) AND !empty($mdpconnect)){
+ $admin = "SELECT * FROM Administrateur WHERE Email = '$mailconnect' AND MOT_DE_PASSE = '$mdpconnect'";
+ $result = mysqli_query($connection,$admin);
+ $row = mysqli_num_rows($result);
+ 
+ if ($row > '0'){
+     $_SESSION['NOM'] = $nom;
+     $_SESSION['PRENOMS'] = $prenom;
+    header("location: enregistrement.php");
+ }
+ else {
+     echo "Mauvais Email ou mot de passe";
+ }
+ }
+else {
+   
+}
+
+
+ }
+?>
---
+++ inscription.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/ufr.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <img src="images/logo.jpg" alt="">
+                    <h2>UFR/SDS</h2>
+                </div>
+                <div class="col-6">
+                    <p>Créer un compte administrateur pour enregistrer <br>et lister les informations des etudiants de l'UFR/SDS</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+    <div class="container">
+            <div class="row">
+
+                <form action="" method="post">
+                    <div class="row mt-4">
+                        <div class="col-2">
+                            <label class="form-label">NOM</label>
+                        </div>
+                        <div class="col-5">
+                            <input type="text" class="form-control" name="nom">
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-2">
+                            <label class="form-label">PRENOMS</label>
+                        </div>
+                        <div class="col-5">
+                            <input type="text" class="form-control" name="prenom">
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-2">
+                            <label class="form-label">Email</label>
+                        </div>
+                        <div class="col-5">
+                            <input type="email" class="form-control" name="mail">
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-2">
+                            <label class="form-label">MOT DE PASSE</label>
+                        </div>
+                        <div class="col-5">
+                            <input type="password" class="form-control" name="mdp">
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-2">
+                            <label class="form-label">CONFIRMER MOT DE PASSE</label>
+                        </div>
+                        <div class="col-5">
+                            <input type="password" class="form-control" name="mdp2">
+                        </div>
+                    </div>
+                    <button class="btn btn-danger mt-4" name="submit">S'INSCRIRE</button>
+                </form>
+
+
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
+
+<?php
+
+include("./dbconnect.php");
+
+if (isset($_POST["submit"])) {
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $mail = $_POST["mail"];
+    $mdp = sha1($_POST["mdp"]);
+    $mdp2 = sha1($_POST["mdp2"]);
+
+    $query = "INSERT INTO `administrateur` (`NOM`, `PRENOMS`, `Email`, `MOT_DE_PASSE`) 
+VALUES ('$nom', '$prenom', '$mail', '$mdp')";
+
+    $result = mysqli_query($connection, $query);
+    if ($mdp != $mdp2) {
+        echo '<script language="javascript"> alert("mot de passe incorrect")</script>';
+    } else {
+        if ($result) {
+            echo '<script language="javascript"> alert("Inscription reussie")</script>';
+        } else {
+
+            echo '<script language="javascript"> alert("Inscription echouée")</script>';
+        }
+    }
+}
+?>
